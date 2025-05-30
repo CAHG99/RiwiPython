6992dfb784d4513a0f088fce001abfbecc1c2021
Cesar Hernandez <cehergra1999@gmail.com>
2025-05-29 19:08:18 -0500

    Delete Cesar/Riwi directory

--- Cesar/Riwi/LECCION02/conceptos/Buscar_elemento.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("clear")
-
-mascotas = ["pelusa","wolfgang","pulga", "felipe", "wolfgang","chanchito feliz"]
-print(mascotas.count("wolfgang"))
-if "wolfgang" in mascotas:
-    print(mascotas.index("wolfgang"))
